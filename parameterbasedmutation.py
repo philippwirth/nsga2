65279e2cf4a884720efceb80a85adc148753847e
philippwirth <pwirth@ethz.ch>
2016-12-07 17:04:42 +0100

    Reformatted comments

--- parameterbasedmutation.py
+++ parameterbasedmutation.py
@@ -3,19 +3,20 @@ import numpy as np
 mutationEta = 2.0
 deltaMax = 1.0
 
+# mutates and individual using parameter based mutation
 def mutate(c):
-
-	n = c.inputDim
-
-	for i in range(n):
-
-		# sample delta from the distribution 0.5*(eta + 1)(1 - delta)^eta
+	
+	n = c.inputDim		# get dimension of search vector
+	
+	for i in range(n):	
+		# sample delta from a distribution chosen to imitate binary crossover
 		u = np.random.rand()
 		if u <= 0.5:
 			delta = pow(2*u, (1/(mutationEta + 1))) - 1
 		else:
 			delta = 1 - pow(2*(1-u), 1/(mutationEta + 1))
 
+		# mutate 
 		c.x[i] += delta*deltaMax
 
 	return c
