04d5629d644e12264dfbcd747770656870eda204
philippwirth <pwirth@ethz.ch>
2016-12-07 17:02:44 +0100

    Reformatted comments

--- nsga2.py
+++ nsga2.py
@@ -11,15 +11,13 @@ import parameterbasedmutation as pbm
 
 
 # options
-_lambda = 128			# number of parents
-mu = 128				# number of children
-inputDim = 1			# search space dimension
-outputDim = 2			# number of objectives
+_lambda = 128		# number of parents
+mu = 128		# number of children
+inputDim = 1		# search space dimension
+outputDim = 2		# number of objectives
 maxGenerations = 100	# maximum number of generations
 
-plotPoints = True
-
-# --------------------------- Individual ---------------------------------#
+# ----------------------------- Individual ---------------------------------#
 class Individual:
 
 	def __init__(self, _x):
@@ -29,7 +27,7 @@ class Individual:
 		self.inputDim = self.x.size
 		self.outputDim = self.fitness.size
 		
-
+		
 	def dominates(self, other):
 		temp = False
 		for i in range(self.outputDim):
@@ -43,17 +41,16 @@ class Individual:
 				temp = True
 
 		return temp
-
-# ----------------------------- NSGA2 -----------------------------------#
-
+	
+# -------------------------------- NSGA2 -----------------------------------#
 
 # initialization
 print('Initialization.')
 
-initialSigma = 5.0						# initial sigma and initial mean determine
+initialSigma = 5.0				# initial sigma and initial mean determine
 initialMean = np.zeros(inputDim)		# the distribution of the initial population
 
-currentPop = []							# create an initial population in initialMean +- 2*initialSigma
+currentPop = []					# create an initial population in initialMean +- 2*initialSigma
 for i in range(_lambda):
 	xi = np.random.rand(inputDim)*4*initialSigma - 2*initialSigma
 	currentPop.append(Individual(xi))
@@ -69,8 +66,8 @@ for g in range(0,maxGenerations):
 	while(nChildren < mu):
 		p0 = currentPop[np.random.randint(mu-1)+1]
 		p1 = currentPop[np.random.randint(mu-1)+1]
-		x0, x1 = sbx.crossover(p0, p1)
-		c0 = pbm.mutate(Individual(x0))
+		x0, x1 = sbx.crossover(p0, p1)			# create two children using simulated binary crossover
+		c0 = pbm.mutate(Individual(x0))			# mutate the children using parameter based mutation
 		c1 = pbm.mutate(Individual(x1))
 		
 		Q.append(p0)
@@ -83,24 +80,9 @@ for g in range(0,maxGenerations):
 	# step 3: selection
 	currentPop = selector.selectBest(Q, _lambda)
 
-	# optional plot
-	if plotPoints:
-		for i in range(_lambda):
-			plt.plot(currentPop[i].fitness[0], currentPop[i].fitness[1], 'wo')
-
 # end
 print('')
 print('Final Population:')
 for i in range(_lambda):
 	print('Individual ', i, ': f(', currentPop[i].x, ') = ', currentPop[i].fitness)
 print('Finishing.')
-
-if plotPoints:
-	for i in range(_lambda):
-		plt.plot(currentPop[i].fitness[0], currentPop[i].fitness[1], 'go')
-
-	plt.xlabel('Objective 1')
-	plt.ylabel('Objective 2')
-	plt.title('Test function')
-	plt.tight_layout()
-	plt.savefig('pareto_front.png')
